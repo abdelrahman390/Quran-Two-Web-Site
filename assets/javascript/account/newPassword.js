999a0d009f4ec40c32a3ea6c191066e9d61821f3
abdelrahman390 <88147091+abdelrahman390@users.noreply.github.com>
2023-08-09 14:31:52 +0300

    Update newPassword.js

--- assets/javascript/account/newPassword.js
+++ assets/javascript/account/newPassword.js
@@ -1,16 +1,25 @@
 
 
-let inputs = document.querySelectorAll('.max-width input')
-let button = document.querySelector('.max-width .container .right a')
+let inputs = document.querySelectorAll('.max-width input'),
+    alarmMessage = document.querySelector('.max-width .alarm'),
+    button = document.querySelector('.max-width .container .right a');
 
 inputs.forEach(element => {
     element.addEventListener('input', function (event) {
 
-        if (inputs[0].value === inputs[1].value) {
-            button.style.cssText = 'opacity: 1; pointer-events: painted'
-            console.log('done')
-        } else if (inputs[0].value !== inputs[1].value) {
-            button.style.cssText = 'opacity: 0.5; pointer-events: none'
+        if (inputs[0].value.length === inputs[1].value.length) {
+
+            if (inputs[0].value === inputs[1].value) {
+                button.style.cssText = 'opacity: 1; pointer-events: painted;';
+                alarmMessage.style.cssText += 'display: none;';
+                inputs[1].style.cssText = 'border: 1px solid var(--primary-color)';
+            } else if (inputs[0].value !== inputs[1].value) {
+                button.style.cssText = 'opacity: 0.5; pointer-events: none;';
+                alarmMessage.style.cssText += 'display: unset;';
+                inputs[1].style.cssText = 'border: 2px solid red';
+            }
+        } else {
+            button.style.cssText = 'opacity: 0.5; pointer-events: none;'
         }
 
     });
