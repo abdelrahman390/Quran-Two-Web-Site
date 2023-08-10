b501b03fd529a1e934114401e582751ab3823757
abdelrahman390 <88147091+abdelrahman390@users.noreply.github.com>
2023-08-10 13:58:06 +0300

    Update cart.js

--- assets/javascript/cart/cart.js
+++ assets/javascript/cart/cart.js
@@ -1,15 +1,30 @@
-let packageRemoveButton = document.querySelectorAll('.cart .max-width .container > .right .box .exit'),
-    deleteButtonOnMessage = document.querySelector('.overLay .container a '),
+var packageRemoveButton = document.querySelectorAll('.cart .max-width .container > .right .box .exit'),
+    deleteButtonOnMessage = document.querySelector('.overLay .container a'),
     overLay = document.querySelector('.overLay '),
-    removeBox = '';
+    removeBox = '',
+    packeges = document.querySelectorAll('.cart .max-width .container > .right .box'),
+    packegesNumber = document.querySelector('.cart .max-width .container > .right > h2 span');
 
 packageRemoveButton.forEach(element => {
     element.onclick = () => {
-        doneMessage('../../assets/images/alarm.svg', 'هل انت متأكد من حذف الباقة', "", 'حذف', "", "done");
+        doneMessage('../../assets/images/alarm.svg', 'هل انت متأكد من حذف الباقة', "#", 'حذف', "", "done");
         removeBox = element.parentElement;
+        removePackage();
     }
 });
 
-deleteButtonOnMessage.onclick = () => {
-    removeBox.remove();
+function removePackage() {
+    deleteButtonOnMessage = document.querySelector('.overLay .container a'),
+        overLay = document.querySelector('.overLay ');
+    deleteButtonOnMessage.addEventListener('click', function () {
+        removeBox.remove();
+        overLay.remove();
+        countPackeges();
+    })
 }
+
+function countPackeges() {
+    packeges = document.querySelectorAll('.cart .max-width .container > .right .box');
+    packegesNumber.innerHTML = packeges.length
+}
+countPackeges()
