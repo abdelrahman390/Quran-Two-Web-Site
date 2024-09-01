f899619fe7d104e85f397bd92b98ab18788b640a
abdelrahman390 <88147091+abdelrahman390@users.noreply.github.com>
2024-09-01 20:31:47 +0300

    Delete assets directory

--- assets/javascript/account/account-subscription.js
+++
@@ -1,7 +0,0 @@
-let packegesDeleteButton = document.querySelectorAll(".account-subscription .max-width .container .lift .box button")
-
-packegesDeleteButton.forEach(element => {
-    element.onclick = () => {
-        doneMessage('../../assets/images/alarm.svg', 'هل انت متأكد من مسح الباقة', "", 'موافق', '', 'done')
-    }
-});
